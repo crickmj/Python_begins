d950b0814a2ccb71d69c2f7a90431e9063ff4a91
MJ Lim <x3gomet@gmail.com>
2022-03-20 20:43:21 +0900

    practice in night

--- practice.py
+++ practice.py
@@ -1,14 +1,3 @@
-from random import *
-
-print(random()) # 0.0 - 1.0 의 임의이 값 생성
-
-print(random() * 10) # 0.0  - 10.0 임의 값 생성 
-
-print(int(random() * 10)) # 0 - 10 미만 임의 int로
-
-print(randrange(1, 46))
-print(randrange(1, 46))
-print(randrange(1, 46))
-print(randrange(1, 46))
-print(randrange(1, 46)) # 1 ~ 46 미만의 임의 값
-print(randint(1,45)) # 1 ~ 45 이하의 임의 값 생성
+python = "Python is Amazing"
+print(python.lower())
+print(python.upper())
