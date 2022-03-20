dd8ea47ff9d3ced9c28c4c9b6559f77e0af2c7e4
MJ Lim <x3gomet@gmail.com>
2022-03-20 20:48:39 +0900

    in night

--- practice.py
+++ practice.py
@@ -1,3 +1,16 @@
 python = "Python is Amazing"
 print(python.lower())
 print(python.upper())
+print(python[0].isupper())
+print(len(python))
+print(python.replace("Python", "Java"))
+
+index = python.index("n")
+print(index)
+
+index = python.index("n", index+1)
+print(index)
+
+print(python.find("Java"))
+
+print(python.count("n"))
