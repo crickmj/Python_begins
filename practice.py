a10dabecefef405e5f9641e1585f639b80129b0f
MJ Lim <x3gomet@gmail.com>
2022-03-21 22:48:31 +0900

    second

--- practice.py
+++ practice.py
@@ -1,2 +1,28 @@
-# 사전
 
+cabinet = {"a-3":"유재석", "b-100":"김태호"}
+
+print(cabinet["a-3"])
+
+print(cabinet)
+
+cabinet["c-20"] = "조세호" 
+print(cabinet)
+
+cabinet["a-3"] = "종국이"
+print(cabinet)
+
+# 간 손님
+
+del cabinet["a-3"]
+print(cabinet)
+
+# key 들만 출력
+
+print(cabinet.keys())
+
+print(cabinet.values())
+
+print(cabinet.items())
+
+cabinet.clear()
+print(cabinet)
