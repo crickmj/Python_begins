8ba326ddda4a8f74ad4018fbbea19fb3014c65a2
MJ Lim <x3gomet@gmail.com>
2022-03-22 23:50:41 +0900

    final

--- practice.py
+++ practice.py
@@ -18,12 +18,24 @@
 
 
 # 학생들 이름을 길이로 바꾸는 연습 
-students = ["iron man", "thor", "groot"]
-students = [len(i) for i in students]
-print(students)
+#students = ["iron man", "thor", "groot"]
+#students = [len(i) for i in students]
+#print(students)
     
     
 # 학생 이름 대문자로
-students = ["iron man", "thor", "groot"]
-students = [i.upper() for i in students]
-print(students)
+#students = ["iron man", "thor", "groot"]
+#students = [i.upper() for i in students]
+#print(students)
+
+absent = [1,3,5,6,78,8,22,2,55,23,33]
+no_books = [88]
+
+for students in range(1,101):
+    if students in absent:
+        continue
+    elif students in no_books:
+        print("{0}, 너 이새끼 따라와라.".format(no_books))
+        break
+    print("{0} 씨, 책 읽어보세요.".format(students)) 
+        
