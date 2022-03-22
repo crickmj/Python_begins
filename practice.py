c1d246aa5cab4ebb4c0e836125265f332252dc51
MJ Lim <x3gomet@gmail.com>
2022-03-22 23:37:21 +0900

    for 문 추가 연습

--- practice.py
+++ practice.py
@@ -1,8 +1,29 @@
-temp = int(input("기온은 어때요? "))
-if 30 <= temp:
-    print("너무 더워요. 나가지 마세요")
-elif 10 <= temp and temp < 30:
-    print("괜찮은 날씨에요") 
-elif 0 <= temp < 10:
-    print("추워요. 나가지 마세요")
+#absent = [2,5] # 결석
+#no_book = [7] # 책을 깜빡하고 안들고 옴 
+#for student in range(1, 11):  # 1- 10까지 출석번호 학생 있을 때
+#    if student in absent: 
+#        continue # 아래 문장 실행하지 않고, 다음 명령으로 가는 것
+#    elif student in no_book:
+#        print("오늘 수업 여기까지. {0} 은 교무실로 따라와.".format(student))
+#        break
+#    print("{0}야 책을 읽어봐".format(student)) 
+    
+    
+    # 출석번호가 1,2,3,4 앞에 100을 붙이기로 함 --> 101, 102, 103, 104 ... 
+    
+#students = [1,2,3,4,5]
+#print(students)
+#students = [i+100 for i in students]
+#print(students)   
 
+
+# 학생들 이름을 길이로 바꾸는 연습 
+students = ["iron man", "thor", "groot"]
+students = [len(i) for i in students]
+print(students)
+    
+    
+# 학생 이름 대문자로
+students = ["iron man", "thor", "groot"]
+students = [i.upper() for i in students]
+print(students)
