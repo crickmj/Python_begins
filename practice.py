6a76a7b40661323168214bbacc2e897d2e11516e
MJ Lim <x3gomet@gmail.com>
2022-03-22 23:00:07 +0900

    else

--- practice.py
+++ practice.py
@@ -2,8 +2,15 @@ weather = "비"
 #if 조건: 
 #  실행 명령문
 
- if weahter == "비":
- print("우산을 챙기세요")
+ 
+ if weather == "비":
+     print("우산을 챙기세요")
+    
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
+else: 
+    print ("준비물 필요없어요")
+
 
 
     
