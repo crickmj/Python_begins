1ca2c49ce2a1a6e534b778c08f1698d0dc790f34
MJ Lim <x3gomet@gmail.com>
2022-03-22 23:17:23 +0900

    elif

--- practice.py
+++ practice.py
@@ -1,9 +1,8 @@
-weather = input("오늘 날씨는 어때요? ")
+temp = int(input("기온은 어때요? "))
+if 30 <= temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10 <= temp and temp < 30:
+    print("괜찮은 날씨에요") 
+elif 0 <= temp < 10:
+    print("추워요. 나가지 마세요")
 
-if weather =="비":
-    print("우산을 챙기세요")
-    
-elif weather =="미세먼지":
-    print("마스크를 챙기세요")    
-else: 
-    print("준비물은 없어요.")
