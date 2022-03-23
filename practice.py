135e3723efbad0f70febecaca4acb6ef19a9f9ca
MJ Lim <x3gomet@gmail.com>
2022-03-23 21:15:11 +0900

    for 연습문!

--- practice.py
+++ practice.py
@@ -8,15 +8,15 @@
 # 출력문 예제
 # 1번째 손님 ( 소요시간 : 15분 )
 # 2버째 손님 (소요시간 : 50분) ..
-# ..
+# 
 # 50번째 손님 (소요시간 16분) 
 # 총 탑승 승객 2명
 
 from random import *
 cnt = 0 # 총 탑승 승객
-for i in range(1,51):
+for i in range(1, 51):
     time = randrange(5, 51) 
-    if 5 <= time <=15:
+    if 5 <= time <= 15:
         print("[0] {0}번째 손님 (소요시간 {1})".format(i, time))
         cnt += 1 
     else: 
