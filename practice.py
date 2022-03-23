c660b83a297cb7211025833206145035bc66cbbf
MJ Lim <x3gomet@gmail.com>
2022-03-23 21:12:17 +0900

    first

--- practice.py
+++ practice.py
@@ -1,41 +1,27 @@
-#absent = [2,5] # 결석
-#no_book = [7] # 책을 깜빡하고 안들고 옴 
-#for student in range(1, 11):  # 1- 10까지 출석번호 학생 있을 때
-#    if student in absent: 
-#        continue # 아래 문장 실행하지 않고, 다음 명령으로 가는 것
-#    elif student in no_book:
-#        print("오늘 수업 여기까지. {0} 은 교무실로 따라와.".format(student))
-#        break
-#    print("{0}야 책을 읽어봐".format(student)) 
-    
-    
-    # 출석번호가 1,2,3,4 앞에 100을 붙이기로 함 --> 101, 102, 103, 104 ... 
-    
-#students = [1,2,3,4,5]
-#print(students)
-#students = [i+100 for i in students]
-#print(students)   
 
+# 당신은 cocoa 서비스를 이용하는 택시 기사님 입니다.
+# 50 명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램
 
-# 학생들 이름을 길이로 바꾸는 연습 
-#students = ["iron man", "thor", "groot"]
-#students = [len(i) for i in students]
-#print(students)
+#조건 1: 승객 별 운행 소요 시간은 5 - 50 분 사이 난수
+#조건 2: 당신은 소요 시간 5 - 15분 사이의 승객만 매칭해야 합니다.
+
+# 출력문 예제
+# 1번째 손님 ( 소요시간 : 15분 )
+# 2버째 손님 (소요시간 : 50분) ..
+# ..
+# 50번째 손님 (소요시간 16분) 
+# 총 탑승 승객 2명
+
+from random import *
+cnt = 0 # 총 탑승 승객
+for i in range(1,51):
+    time = randrange(5, 51) 
+    if 5 <= time <=15:
+        print("[0] {0}번째 손님 (소요시간 {1})".format(i, time))
+        cnt += 1 
+    else: 
+        print("[0] {0}번째 손님 (소요시간 {1})".format(i, time))
     
-    
-# 학생 이름 대문자로
-#students = ["iron man", "thor", "groot"]
-#students = [i.upper() for i in students]
-#print(students)
+print("총 탑승 승객: {0}분".format(cnt))
 
-absent = [1,3,5,6,78,8,22,2,55,23,33]
-no_books = [88]
-
-for students in range(1,101):
-    if students in absent:
-        continue
-    elif students in no_books:
-        print("{0}, 너 이새끼 따라와라.".format(no_books))
-        break
-    print("{0} 씨, 책 읽어보세요.".format(students)) 
         
