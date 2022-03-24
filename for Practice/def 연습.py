a12e995b166528201b338636e57a592a02ffbf5d
MJ Lim <x3gomet@gmail.com>
2022-03-24 23:10:58 +0900

    def  연습

--- for Practice/def 연습.py
+++ for Practice/def 연습.py
@@ -26,3 +26,30 @@ balance = deposit(balance, 1000)
 commision, balance = withdraw_night(balance,500)
 
 print("수수료는 {0}원 이며, 잔액은 {1}원 입니다.".format(commision, balance))
+
+
+# 표준 체중을 구하는 프로그램을 만드시오
+
+# 표준체중: 각 개인의 키에 적당한 체중
+# 성별에 따른 공식
+# 남자: 키 x 키 x 22
+# 여자: 키 x 키 x 21 
+
+# 조건: 표준 체중은 별도의 함수 내에서 계산
+# * 함수명: std_weight 
+# * 전달값: 키(height), 성별(gender)
+
+#조건 2: 표준 체중은 소수점 둘째자리 까지 표시
+
+# 출력예제 : 키 175 cm 남자의 표준 체중은 67.38 kg 입니다. 
+
+def std_weight(height, gender): 
+    if gender == "남자":
+        return height * height * 22
+    else:
+        return height * height * 21
+    
+height = 175  # cm 
+gender = "남자" 
+weight = round(std_weight(height/100, gender),2)
+print("키 {0} cm {1}의 표준 체중은 {2}kg 입니다.".format(height, gender, weight))
