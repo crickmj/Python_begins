b6c85e8840181be9913ec621384d9f110bafc8ec
MJ Lim <x3gomet@gmail.com>
2022-03-27 19:01:26 +0900

    ;;

--- for Practice/practice_24Mar22.py
+++ for Practice/practice_24Mar22.py
@@ -1,15 +1,7 @@
-# scores = {"수학":0, "영어":50, "코딩":100}
+from re import L
 
-# for subject, score in scores.items(): 
-#    print(subject.ljust(8), str(score).rjust(4), sep=":")
+
+scores = {"수학":0, "영어":50, "코딩":100}
+for subject in scores.items():
+    print(subject.ljust,scores)
     
-
-
-# 은행 대기 순번표
-# 001, 002, 003, ...
-
-# for number in range(1, 21):
-#     print("대기번호: " + str(number).zfill(3))
-
-# answer = input("아무값이나 입력하세요 : ") : # input사용자 입력 하면 무조건 str로 나오니까 확인 ! 
-# print("입력하신 값은 " + answer + "입니다")
