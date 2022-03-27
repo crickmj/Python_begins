964563ee4e19ec0745fc26810e148b4500d4ccde
MJ Lim <x3gomet@gmail.com>
2022-03-27 19:43:36 +0900

    update

---
+++ for Practice/27Mar22_pkpk.py
@@ -0,0 +1,12 @@
+
+# profile_file = open("profile.pickle", "wb") # pickle 에서는 binary 설정을 위해 wb로 입력, pickle은 따로 encoding 설정 필요 없음
+# profile = {"이름":"박명수","나이":30,"취미":["축구","골프","코딩"]}
+# print(profile)
+# pickle.dump(profile, profile_file) # profile에 있는 정보를 file에 저장
+# profile_file.close()
+
+import pickle
+profile_file = open("profile.pickle", "rb")
+profile = pickle.load(profile_file) #파일에 있는 정보 profile에 불러오기
+print(profile)
+profile_file.close()
