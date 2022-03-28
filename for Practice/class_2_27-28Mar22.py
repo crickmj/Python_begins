1c6f134e8e675144fc6783211738904b5a819309
MJ Lim <x3gomet@gmail.com>
2022-03-28 22:56:56 +0900

    oh..

--- for Practice/class_2_27-28Mar22.py
+++ for Practice/class_2_27-28Mar22.py
@@ -1,14 +1,21 @@
 # 상속! 
 # 일반 유닛 
 
+from turtle import speed
+
+
 class Unit: # 부모 클래스 
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+    def move(self, location):
+        print("[지상 공격 유닛]")
+        print("{0} 유닛: {1} 시 방향으로 {2} 의 속도로 이동합니다.".format(self.name, location, self.speed))
       
 class AttackUnit(Unit): # 자식 클래스 
-    def __init__(self, name, hp, damage):
-        Unit.__init__(self,name,hp)
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self,name,hp,speed)
         self.damage = damage
     
     def attack(self, location):
@@ -38,12 +45,25 @@ class Flyable: # 날 수 있는 클래스
   
 class FlyableAttachUnit(AttackUnit, Flyable): # 공격 및 날 수 있는 거 모두 상속
     def __init__(self,name,hp,damage,flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0, damage) # 지상 스피드는 0으로 처리 
         Flyable.__init__(self,flying_speed) 
-
+    
+    def move(self, location):
+        print("[공중 유닛 이동]")
+        self.fly(self.name, location)
+        
 # 발키리: 공중 공격, 한 번에 14발 
 
 Valkyrie = FlyableAttachUnit("발키리", 200, 6, 5)
 Valkyrie.fly(Valkyrie.name, "3")
 
-# 연산자 오버로딩
+# 메소드 오버로딩..! --> 지상유닛 스피드 추가
+
+#벌쳐 추가 : 무브 있음
+vulture = AttackUnit("벌쳐", 80, 10, 20)
+# 배틀크루져 : 무브 없음
+Battlecruiser = FlyableAttachUnit("배틀쿠르져", 500, 25, 3)
+
+vulture.move("11")
+Battlecruiser.move("9") # 지상/공중 인 것 구분해야해서.. 메소드 오벌됭 해야함
+
