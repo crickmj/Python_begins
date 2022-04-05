0aa882bffabd0798d241c14a802044bb8c05cbf4
MJ Lim <x3gomet@gmail.com>
2022-04-05 20:00:46 +0900

    0405

--- for Practice/Jump_to_python/220405_input_output.py
+++ for Practice/Jump_to_python/220405_input_output.py
@@ -1,7 +1,7 @@
 # file 생성하기
+f1 = open("new_file.t", 'w', encoding = "utf8")
+for i in range (1,11):
+    data = "%d 번 째 줄입니다. \n" % i
+    f1.write(data)
+f1.close()
 
-from encodings import utf_8
-
-
-f = open("file.txt", "w", encoding = "utf8")
-f.close()
