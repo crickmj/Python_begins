7911282adc2d8cc1a0e04551815dc5cec4c921e0
MJ Lim <x3gomet@gmail.com>
2022-04-09 22:59:22 +0900

    def

--- for Practice/Jump_to_python/220409_exception.py
+++ for Practice/Jump_to_python/220409_exception.py
@@ -109,3 +109,38 @@ print(os.getcwd())
 
 import shutil
 shutil.copy('new.txt','shutil.txt')
+
+# timesleep
+
+# import time
+# for i in range(10):
+#     print(i)
+#     time.sleep(0.3)
+    
+# import calendar
+# print(calendar.calendar(2022))
+
+# import webbrowser
+# webbrowser.open("http://www.naver.com")
+
+import time
+import threading
+
+def long_task():
+    for i in range(5):
+        time.sleep(1)
+        print("working:%s\n" % i)
+
+print("Start")
+
+threads = []
+
+for i in range(5):
+    t = threading.Thread(target=long_task)
+    threads.append(t)
+for t in threads:
+    t.start()
+for t in threads:
+    t.join()
+    
+print("End")
